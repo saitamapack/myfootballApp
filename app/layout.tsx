2eabe2123530d826caf88a9981c88345982b640c
saitamapack <97965030+saitamapack@users.noreply.github.com>
2024-07-15 12:53:30 +0000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,11 +20,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <div className='relative bg-black'>
           <div className='absolute top-0 left-0 w-full h-full'>
-            <img
-              src='/background.png'
-              alt='backgroundImage'
-              className='h-screen w-full object-cover'
-            />
+            
           </div>
           <div className='absolute top-0 left-0 w-full h-screen bg-gradient-to-b
                           from-black/10 to-black'/>
